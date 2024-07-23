da73d10c22b3e042bda6619b7e7e60e7fcf83a55
cyy <1741084416@qq.com>
2024-07-23 10:21:38 +0800

    push server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//New imports
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+// New imports
+// .....
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+            socket.disconnect()
+      console.log('🔥: A user disconnected');
+    });
+});
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Hello world",
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
